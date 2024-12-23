3065642065c0107ed4b6b9b084a83e1a7448e9c9
XBold <80431693+XBold@users.noreply.github.com>
2024-12-23 23:30:04 +0100

    Aggiungere i file di progetto.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AABridgeWireless
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
